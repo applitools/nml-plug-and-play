1253f87f013f4d0fc30a8be4aef152ab684cd7d6
ssharma007-dev <shreya.sharma@applitools.com>
2026-07-10 13:42:39 +0530

    Update accessibility test: add Main Screen | Fully check

--- test/specs/ios/accessibility.ios.test.ts
+++ test/specs/ios/accessibility.ios.test.ts
@@ -26,6 +26,8 @@ describe('Accessibility iOS NML - LambdaTest', () => {
     await eyes.check('Main Screen', Target.window());
     console.log('Checked: Main Screen');
 
+    await eyes.check('Main Screen | Fully', Target.window().fully());
+
     await eyes.close(false);
     console.log('Eyes closed');
   });
