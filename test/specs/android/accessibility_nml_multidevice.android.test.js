255bd14284a3792247bf95b69d7d6c834f29212f
Shreya Sharma <shreya.sharma@applitools.com>
2026-07-17 15:41:02 +0530

    Use ClassicRunner + eyes_local.log everywhere

--- test/specs/android/accessibility_nml_multidevice.android.test.js
+++ test/specs/android/accessibility_nml_multidevice.android.test.js
@@ -1,10 +1,11 @@
-import { Eyes, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
+import { Eyes, ClassicRunner, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
 
 describe('Accessibility Android NML - Local', () => {
-  let eyes;
+  let eyes, runner;
 
   before(async () => {
-    eyes = new Eyes();
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
     eyes.setLogHandler({ type: 'file', filename: './logs/eyes_local.log' });
 
     const config = new Configuration();
