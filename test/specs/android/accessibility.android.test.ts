c6ada8788b17781d3060588a4784bb9a44453604
Shreya Sharma <shreya.sharma@applitools.com>
2026-07-17 15:40:44 +0530

    Use ClassicRunner + eyes_lambdatest.log everywhere

--- test/specs/android/accessibility.android.test.ts
+++ test/specs/android/accessibility.android.test.ts
@@ -1,10 +1,12 @@
-import { Eyes, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
+import { Eyes, ClassicRunner, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
 
 describe('Accessibility Android NML - LambdaTest', () => {
   let eyes: Eyes;
+  let runner: ClassicRunner;
 
   before(async () => {
-    eyes = new Eyes();
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
     eyes.setLogHandler({ type: 'file', filename: './logs/eyes_lambdatest.log' });
 
     const config = new Configuration();
